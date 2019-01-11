af80afe2a3344e4128d01773fc71a60c79cffda9
reubenwalker64 <rwalker@mobile-atom.com>
2019-01-11 13:32:07 -0500

    3rd commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,26 @@
+/*Before you can make any part of our site work, you need to create an array of strings, each one related to a topic that interests you. Save it to a variable called topics.
+
+We chose animals for our theme, but you can make a list to your own liking.
+Your app should take the topics in this array and create buttons in your HTML.
+
+Try using a loop that appends a button for each string in the array.
+When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
+
+When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
+
+Under every gif, display its rating (PG, G, so on).
+
+This data is provided by the GIPHY API.
+Only once you get images displaying with button presses should you move on to the next step.
+Add a form to your page takes the value from a user input box and adds it into your topics array. Then make a function call that takes each topic in the array remakes the buttons on the page.
+
+*/
 
 //Look at Dynamic Elements activity and Button Triggered Ajax activity
 
 // Giphy Api Key:734LB9IAWwIgnKyEvKjRSj8pUKFwU5Oo sub into below
+
+$(document).ready(function(){
 
 //EDIT BELOW
   // Adding click event listen listener to all buttons
@@ -182,3 +201,5 @@ $("#run-search").on("click", function(event) {
   //  .on("click") function associated with the clear button
   $("#clear-all").on("click", clear);
   
+
+});
